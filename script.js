d6c16b583584ee6b4c456252a8c297e1933d0395
geropython <72422102+geropython@users.noreply.github.com>
2025-02-18 15:58:51 -0300

    principio

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const loginForm = document.getElementById("loginForm");
+    const loginEmail = document.getElementById("loginEmail");
+    const loginPassword = document.getElementById("loginPassword");
+    const loginConfirmPassword = document.getElementById("loginConfirmPassword");
+});//escucha a penas cargue
